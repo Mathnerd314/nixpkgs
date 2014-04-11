ba0bfcd832ab27d9e4589b0c093c6f9bf8ef7ae7
Alexander Kjeldaas <ak@formalprivacy.com>
2014-04-11 10:06:07 +0200

    When useFakeTime, set FAKETIME unless set.

--- pkgs/development/libraries/libfaketime/hook.nix
+++ pkgs/development/libraries/libfaketime/hook.nix
@@ -10,6 +10,13 @@ stdenv.mkDerivation {
       # FAKETIME_ONLY_CMDS="gcc,date"
       if [ "$NIX_ENFORCE_PURITY" = "1" ]; then
           makePreloads += (LD_PRELOAD=${libfaketime}/lib/libfaketime.so.1)
+          if [ -z "$FAKETIME" ]; then
+              export FAKETIME="1970-01-01 00:00:01"
+          fi
+          echo "using libfaketime: FAKETIME_SKIP_CMDS:'$FAKETIME_SKIP_CMDS'"
+          echo "using libfaketime: FAKETIME_ONLY_CMDS:'$FAKETIME_ONLY_CMDS'"
+          echo "using libfaketime: FAKETIME_ONLY_CMDS:'$FAKETIME_ONLY_CMDS'"
+          echo "using libfaketime: FAKETIME:'$FAKETIME'"
       fi
     }
     
