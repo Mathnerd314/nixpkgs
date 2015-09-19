80f867d05ee3f35b4feb44aac4cb6cfaf178b3fd
Mathnerd314 <mathnerd314.gph+hs@gmail.com>
2015-09-19 17:31:34 -0600

    Try #1

--- maintainers/scripts/travis-nox-review-pr.sh
+++ maintainers/scripts/travis-nox-review-pr.sh
@@ -2,16 +2,26 @@
 set -e
 
 export NIX_CURL_FLAGS=-sS
+export NIX_CONF_DIR=/tmp/etc/nix
 
 if [[ $1 == nix ]]; then
+    which fakechroot
+    mkdir -p $HOME/root/etc
+    for i in /etc/*; do
+      ln -s $i $HOME/root/$i
+    done
     echo "=== Installing Nix..."
-    # Install Nix
-    bash <(curl -sS https://nixos.org/nix/install)
+    cd $HOME/root
+    mkdir nix
+    export FAKECHROOT_EXCLUDE_PATH=/bin:/dev:/home:/lib64:/mnt:/opt:/root:/sbin:/srv:/tmp:/var:/boot:/lib:/media:/nonexistent:/proc:/run:/selinux:/sys:/usr
+    fakechroot chroot . bash <(curl -sS https://nixos.org/nix/install)
+    ls -R .
+    strace $HOME/root/nix/store/na9pnc5pj9d63xn5z8v4wbc35fm4m9y1-nix-1.10/bin/nix-store
     source $HOME/.nix-profile/etc/profile.d/nix.sh
 
     # Make sure we can use hydra's binary cache
-    sudo mkdir /etc/nix
-    sudo tee /etc/nix/nix.conf <<EOF >/dev/null
+    mkdir $NIX_CONF_DIR
+    mkdir tee $NIX_CONF_DIR/nix.conf <<EOF >/dev/null
 binary-caches = http://cache.nixos.org http://hydra.nixos.org
 trusted-binary-caches = http://hydra.nixos.org
 build-max-jobs = 4
@@ -21,9 +31,13 @@ EOF
     echo "=== Verifying that nixpkgs evaluates..."
     nix-env -f. -qa --json >/dev/null
 elif [[ $1 == nox ]]; then
-    echo "=== Installing nox..."
-    git clone -q https://github.com/madjar/nox
-    pip --quiet install -e nox
+    if [[ $TRAVIS_PULL_REQUEST == false ]]; then
+        echo "=== Skipping nox"
+    else
+        echo "=== Installing nox..."
+        git clone -q https://github.com/madjar/nox
+        pip --quiet install -e nox
+    fi
 elif [[ $1 == build ]]; then
     source $HOME/.nix-profile/etc/profile.d/nix.sh
 
