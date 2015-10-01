6247d96ea824d60f00c31f579d90c944e4319a3a
Mathnerd314 <mathnerd314.gph+hs@gmail.com>
2015-09-30 19:05:18 -0600

    Guix stuff

--- pkgs/tools/package-management/guix/default.nix
+++ pkgs/tools/package-management/guix/default.nix
@@ -1,45 +1,43 @@
-{ fetchurl, stdenv, guile, libgcrypt, sqlite, bzip2, pkgconfig }:
+{ fetchurl, stdenv, guile, libgcrypt, sqlite, bzip2, pkgconfig, gettext, autoconf, automake, lib }:
 
 let
   # Getting the bootstrap Guile binary.  This is normally performed by Guix's build system.
   base_url = arch:
-    "http://alpha.gnu.org/gnu/guix/bootstrap/${arch}-linux/20130105/guile-2.0.7.tar.xz";
+    "http://alpha.gnu.org/gnu/guix/bootstrap/${arch}-linux/20131110/guile-2.0.9.tar.xz";
   boot_guile = {
     i686 = fetchurl {
       url = base_url "i686";
-      sha256 = "f9a7c6f4c556eaafa2a69bcf07d4ffbb6682ea831d4c9da9ba095aca3ccd217c";
+      sha256 = "0im800m30abgh7msh331pcbjvb4n02smz5cfzf1srv0kpx3csmxp";
     };
     x86_64 = fetchurl {
       url = base_url "x86_64";
-      sha256 = "bc43210dcd146d242bef4d354b0aeac12c4ef3118c07502d17ffa8d49e15aa2c";
+      sha256 = "1w2p5zyrglzzniqgvyn1b55vprfzhgk8vzbzkkbdgl5248si0yq3";
     };
   };
 in stdenv.mkDerivation rec {
-  name = "guix-0.3";
+  name = "guix-0.8.3";
 
   src = fetchurl {
-    url = "ftp://alpha.gnu.org/gnu/guix/${name}.tar.gz";
-    sha256 = "0xpfdmlfkkpmgrb8lpaqs5wxx31m4jslajs6b9waz5wp91zk7fix";
+    url = "http://alpha.gnu.org/gnu/guix/${name}.tar.gz";
+    sha256 = "14n0nkj0ckhdwhghx1pml99hbjr1xdkn8x145j0xp1357vqlisnz";
   };
 
   configureFlags =
-     [ "--localstatedir=/nix/var"
-       "--with-libgcrypt-prefix=${libgcrypt}"
+     [ "--with-libgcrypt-prefix=${libgcrypt}"
      ];
 
   preBuild =
     # Copy the bootstrap Guile tarballs like Guix's makefile normally does.
-    '' cp -v "${boot_guile.i686}" gnu/packages/bootstrap/i686-linux/guile-2.0.7.tar.xz
-       cp -v "${boot_guile.x86_64}" gnu/packages/bootstrap/x86_64-linux/guile-2.0.7.tar.xz
+    '' cp -v "${boot_guile.i686}" gnu/packages/bootstrap/i686-linux/guile-2.0.9.tar.xz
+       cp -v "${boot_guile.x86_64}" gnu/packages/bootstrap/x86_64-linux/guile-2.0.9.tar.xz
     '';
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ guile libgcrypt sqlite bzip2 ];
+  buildInputs = [ autoconf automake pkgconfig guile libgcrypt sqlite bzip2 gettext ];
 
   doCheck = true;
   enableParallelBuilding = true;
 
-  meta = {
+  meta = with lib; {
     description = "Functional package manager with a Scheme interface";
 
     longDescription = ''
@@ -59,10 +57,10 @@ in stdenv.mkDerivation rec {
       Guix is based on the Nix package manager.
     '';
 
-    license = stdenv.lib.licenses.gpl3Plus;
+    license = licenses.gpl3Plus;
 
-    maintainers = [ ];
-    platforms = stdenv.lib.platforms.linux;
+    maintainers = with maintainers; [ mathnerd314 ];
+    platforms = platforms.linux;
 
     homepage = http://www.gnu.org/software/guix;
   };
