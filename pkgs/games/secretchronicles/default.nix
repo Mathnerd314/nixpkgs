0035ca1bc93626fc915bb193b6c8250e17a1f254
Mathnerd314 <mathnerd314.gph+hs@gmail.com>
2015-10-11 11:57:38 -0600

    secretchronicles: more work on build

--- pkgs/games/secretchronicles/default.nix
+++ pkgs/games/secretchronicles/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, ruby, gperf, pkgconfig, bison, glew, freeglut, gettext, libpng, SDL, SDL_ttf, SDL_mixer, SDL_image, pcre, libxmlxx, freetype, libdevil, boost, cmake, xlibs, glibmm }:
+{ stdenv, lib, fetchurl, ruby, gperf, pkgconfig, bison, glew, freeglut, gettext, libpng, SDL, SDL_ttf, SDL_mixer, SDL_image, pcre, libxmlxx, freetype, libdevil, boost, cmake, xlibs, glibmm }:
 let
   version = "2.0.0";
 in
@@ -11,18 +11,12 @@ stdenv.mkDerivation {
   };
 
   buildInputs = [ ruby gperf pkgconfig bison glew freeglut gettext libpng SDL SDL_ttf SDL_mixer SDL_image pcre libxmlxx freetype libdevil boost cmake xlibs.libX11 glibmm ];
-  
 
-
-  configurePhase = ''
-    cmake -DCMAKE_INSTALL_PREFIX=$out
-  '';
-
-  meta = {
-    description = "The Secret Chronicles of Dr. M is a 2D jump n' run platformer.";
-    license = stdenv.lib.licenses.gpl3;
-    homepage = "http://secretchronicles.de/";
-    maintainers = [ stdenv.lib.maintainers.mathnerd314 ];
-    platforms = stdenv.lib.platforms.linux;
+  meta = with lib; {
+    description = "The Secret Chronicles of Dr. M, a two-dimensional sidecrolling platform game";
+    license = licenses.gpl3;
+    homepage = http://www.secretchronicles.de/en/;
+    maintainers = [ maintainers.mathnerd314 ];
+    platforms = platforms.linux;
   };
 }
