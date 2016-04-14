ba799df5cc8333cf3c3c0dc332279e505576aa69
Mathnerd314 <mathnerd314.gph+hs@gmail.com>
2016-04-14 13:51:27 -0600

    pyusb: 1.0.0b2 -> 1.0.0rc1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -25066,7 +25066,7 @@ in modules // {
     postPatch = ''
       libusb=${pkgs.libusb1.out}/lib/libusb-1.0.so
       test -f $libusb || { echo "ERROR: $libusb doesn't exist, please update/fix this build expression."; exit 1; }
-      sed -i -e "s|libname = .*|libname = \"$libusb\"|" usb/backend/libusb1.py
+      sed -i -e "s|lib = locate_library.*|lib = \"$libusb\"|" usb/libloader.py
     '';
 
     # No tests included
