b18e5c67237e7597f95c74c9a7790fedf2951767
Mathnerd314 <mathnerd314.gph+hs@gmail.com>
2016-04-28 23:47:45 -0600

    Clean up /default.nix

--- default.nix
+++ default.nix
@@ -7,33 +7,31 @@ if ! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.
 else
   /* Impure default arguments for `pkgs/top-level/default.nix`. See that file
     for the meaning of each argument. */
-  { system ? builtins.currentSystem, config ? null, ... } @ args :
-    let config_ = config; in # rename the function arguments
+  args @ { system ? builtins.currentSystem
+         , config ?
+            # The contents of the configuration file found at $NIXPKGS_CONFIG or
+            # $HOME/.nixpkgs/config.nix.
+            # for NIXOS (nixos-rebuild): use nixpkgs.config option to set
+            let
+              toPath = builtins.toPath;
+              getEnv = x: if builtins ? getEnv then builtins.getEnv x else "";
+              pathExists = name:
+                builtins ? pathExists && builtins.pathExists (toPath name);
+
+              configFile = getEnv "NIXPKGS_CONFIG";
+              homeDir = getEnv "HOME";
+              configFile2 = homeDir + "/.nixpkgs/config.nix";
+            in
+              if configFile != "" && pathExists configFile then import (toPath configFile)
+              else if homeDir != "" && pathExists configFile2 then import (toPath configFile2)
+              else {}
+          , ... }:
+    let configExpr = config; in # rename the function arguments
     let
-      # The contents of the configuration file found at $NIXPKGS_CONFIG or
-      # $HOME/.nixpkgs/config.nix.
-      # for NIXOS (nixos-rebuild): use nixpkgs.config option
+      # allow both:
+      # { /* the config */ } and
+      # { pkgs, ... } : { /* the config */ }
       config =
-        let
-          toPath = builtins.toPath;
-          getEnv = x: if builtins ? getEnv then builtins.getEnv x else "";
-          pathExists = name:
-            builtins ? pathExists && builtins.pathExists (toPath name);
-
-          configFile = getEnv "NIXPKGS_CONFIG";
-          homeDir = getEnv "HOME";
-          configFile2 = homeDir + "/.nixpkgs/config.nix";
-
-          configExpr =
-            if config_ != null then config_
-            else if configFile != "" && pathExists configFile then import (toPath configFile)
-            else if homeDir != "" && pathExists configFile2 then import (toPath configFile2)
-            else {};
-
-        in
-          # allow both:
-          # { /* the config */ } and
-          # { pkgs, ... } : { /* the config */ }
           if builtins.isFunction configExpr
             then configExpr { inherit pkgs; }
             else configExpr;
